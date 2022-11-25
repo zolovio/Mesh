882a398c54952f2993a935b139795d443136d37f
Ahtsham0715 <ahtsham50743@gmail.com>
2022-11-25 16:18:48 +0500

    fixes

--- lib/feature/auth/ui/verify/verify_screen.dart
+++ lib/feature/auth/ui/verify/verify_screen.dart
@@ -102,7 +102,7 @@ class _VerifyScreenState extends ConsumerState<VerifyScreen> {
                                     ),
                                     TextSpan(
                                         text:
-                                            "+92 ${widget.verifyModel.mobileNumber}",
+                                            "+91 ${widget.verifyModel.mobileNumber}",
                                         style: const TextStyle(
                                             fontWeight: FontWeight.w600,
                                             color: Colors.black)),
